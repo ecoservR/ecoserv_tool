e54b6c086832202b9c7bb159f85dd8a24d5445f1
sandra-ab <sandra_angers_b@hotmail.com>
2023-03-01 13:27:41 +0000

    fix missing crs and grid crs issues

--- R/mod08_add_DTM.R
+++ R/mod08_add_DTM.R
@@ -51,10 +51,8 @@ add_DTM <- function(mm = parent.frame()$mm,
 
       ## If no projection set, flash a warning and assign OSGB1936
 
-      if (is.na(raster::crs(dtm[[1]]))){
-
-         message("Warning! No projection set for your DTM so we will assume it is OSGB 1936 (British National Grid).")
-
+      if (any(unlist(lapply(dtm, function(x) is.na(raster::crs(x)))))){
+      message("Warning! No projection set for your DTM so we will assume it is OSGB 1936 (British National Grid).")
 
          dtm <- lapply(dtm, function(x){
             suppressWarnings({
@@ -64,6 +62,20 @@ add_DTM <- function(mm = parent.frame()$mm,
          })
 
       }
+
+      # if (is.na(raster::crs(dtm[[1]]))){
+      #
+      #    message("Warning! No projection set for your DTM so we will assume it is OSGB 1936 (British National Grid).")
+      #
+      #
+      #    dtm <- lapply(dtm, function(x){
+      #       suppressWarnings({
+      #          raster::crs(x) <- sp::CRS(SRS_string = "EPSG:27700")
+      #          return(x)
+      #       })
+      #    })
+      #
+      # }
 
       # Calculate side of square (assuming projection is in meters)
       span <- dim(dtm[[1]])[[1]]*raster::res(dtm[[1]])[[1]]
@@ -76,7 +88,7 @@ add_DTM <- function(mm = parent.frame()$mm,
 
          # we can call the object "grid" directly
 
-         gridSA <- suppressWarnings(sf::st_intersection(grid, studyAreaBuffer))  # create gridded study area
+         gridSA <- suppressWarnings(sf::st_intersection(sf::st_set_crs(ecoservR::grid, 27700), studyAreaBuffer))  # create gridded study area
 
          gridSA$TILE_NAME <- droplevels(gridSA$TILE_NAME)  # drop squares that are not in the study area
 
@@ -115,7 +127,7 @@ add_DTM <- function(mm = parent.frame()$mm,
          rcentro <- do.call(rbind, rcentro)  # make into sf df
 
          # index which grid tiles each raster belongs to (several rasters will belong to one tile)
-         rtiles <- sf::st_intersects(rcentro, ecoservR::grid)
+         rtiles <- sf::st_intersects(rcentro, sf::st_set_crs(ecoservR::grid, 27700))
 
          # add the 10km grid ref as names to the rasters
          # This could crash if a rcentro point is exactly across two tiles, but shouldn't be if dtm from any reputable source
